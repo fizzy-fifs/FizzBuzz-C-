9e34c35003d57f4bd180c6c3d60b6d1cb315e2b2
Fifolu Akinyemi <fkinyemi@gmail.com>
2021-11-29 14:27:22 +0000

    FizzBuzz lists numbers up to one

---
+++ Core/FizzBuzz.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Core
+{
+    public class FizzBuzz
+    {
+        public string generate(int count)
+        {
+            return count.ToString();
+        }
+    }
+}
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ListNumbersUpToOne()
+        {
+            FizzBuzz fizzbuzz = new();
+            string result = fizzbuzz.generate(1);
+            Assert.AreEqual("1", result);
+        }
+
+        [TestMethod]
+        public void ListNumbersUpToTwo()
+        {
+            FizzBuzz fizzbuzz = new();
+            string result = fizzbuzz.generate(2);
+            Assert.AreEqual("1, 2", result);
+        }
+    }
+}
